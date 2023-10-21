d3eaf9e8349a67366ef07620c6d775bd8add4b29
demiapollo <apollodemirel67@gmail.com>
2023-10-20 20:03:46 -0700

    higher lower game completed

--- higher_lower.py
+++ higher_lower.py
@@ -1,28 +1,60 @@
 from game_data import data
 from art import logo, vs
 import random
+import os
 
-# Randomly select a person1 from game_date 
-max = len(data)
-selected1 = random.randint(0, max)
-selected_person1 = data[selected1]
+def clear():
+    os.system("clear")
 
-# Randomly select a person2 from game_date 
-selected2 = random.randint(0, max)
-selected_person2 = data[selected2]
 
-# Display name and other details of selected people with VS
-name1 = selected_person1["name"]
-description1 = selected_person1["description"]
-country1 = selected_person1["country"]
+def format_account_data(account):
+    """Format the account data into printable format."""
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
 
-name2 = selected_person2["name"]
-description2 = selected_person2["description"]
-country2 = selected_person2["country"]
 
-print(f"Compare A: {name1}, a {description1}, from {country1}.")
-print(vs)
-print(f"Compare A: {name2}, a {description2}, from {country2}.")
+def check_answer(guess, count1, count2):
+    """Take the user guess and follower counts na return if they got it right."""
+    if count1 > count2:
+        return guess == "a"
+    else:
+        return guess == "b"
 
-# Ask the user to choose A or B, indicating who has more IG followers
-answer = input("Who has more Instagram followers? Type 'A' or 'B': ")
+
+print(logo)
+current_score = 0
+game_should_continue = True
+selected2 = random.choice(data)
+
+while game_should_continue:
+    # Randomly select a person1 from game_date 
+    selected1 = selected2
+    selected2 = random.choice(data)
+    
+    while selected1 == selected2:
+        selected2 = random.choice(data)
+
+    # Display name and other details of selected people
+    print(f"Compare A: {format_account_data(selected1)}.")
+    print(vs)
+    print(f"Against B: {format_account_data(selected2)}.")
+
+    # Ask the user to choose A or B, indicating who has more IG followers
+    answer = input("Who has more Instagram followers? Type 'A' or 'B': ").lower()
+
+    # Set the logic for deciding the whether the answer is correct or false
+    follower_count1 = selected1["follower_count"]                                   
+    follower_count2 = selected2["follower_count"]
+    is_correct = check_answer(answer, follower_count1, follower_count2)
+    
+    clear()
+    print(logo)
+
+    if is_correct:
+        current_score += 1
+        print(f"You are right. Current score: {current_score}.")
+    else:
+        game_should_continue = False
+        print(f"Sorry, that's wrong. Final score: {current_score}.")
